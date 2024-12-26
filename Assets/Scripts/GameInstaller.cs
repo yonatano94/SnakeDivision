8a0c94fcccc20a208196fdcfb9b97a5ded718811
UriLootHeads <125879405+UriLootHeads@users.noreply.github.com>
2024-12-26 12:43:06 +0200

    review

--- Assets/Scripts/GameInstaller.cs
+++ Assets/Scripts/GameInstaller.cs
@@ -1,8 +1,13 @@
 using Zenject;
 using UnityEngine;
 
+
+//this installer can be divided into smaller ones  
+//missing namespace 
 public class GameInstaller : MonoInstaller
 {
+    //prefabs live in the project scope and should use ScriptableInstallers instead of MonoInstaller
+    //
     [SerializeField] private GameObject itemPrefab;
     [SerializeField] private GameObject wallPrefab;
     [SerializeField] private GameObject snakeSegmentPrefab;
@@ -10,6 +15,7 @@ public class GameInstaller : MonoInstaller
 
     public override void InstallBindings()
     {
+        //hard coded strings
         Container.Bind<GameObject>().WithId("Item").FromInstance(itemPrefab);
         Container.Bind<GameObject>().WithId("Wall").FromInstance(wallPrefab);
         Container.Bind<GameObject>().WithId("SnakeSegment").FromInstance(snakeSegmentPrefab);
